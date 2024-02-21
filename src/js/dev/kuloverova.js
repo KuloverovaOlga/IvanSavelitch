0a87f52379fc50ec5e9475eb68fca08c99ce5ce6
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-02-21 16:00:09 +0400

    start

---
+++ src/js/dev/kuloverova.js
@@ -0,0 +1,17 @@
+
+import 'inputmask';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+export const modules = {};
+window.$ = window.jQuery = require('jquery');
+import { rem } from '../utils/constants';
+
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+
+  } catch {}
+
+});
+
+
