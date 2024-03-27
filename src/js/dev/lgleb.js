258532142619156c0c672fbe36ada454cac77a4b
Gleb Leus <lv.gleb44@gmail.com>
2024-03-27 16:44:42 +0300

    love block: 27.03.2024

--- src/js/dev/lgleb.js
+++ src/js/dev/lgleb.js
@@ -11,7 +11,24 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.item-banner__weights') && weightTabs();
 
   document.querySelector('.catalog-block') && catalogBannerAnimation();
+
+  document.querySelector('.love') && loveAnimation();
 });
+
+const scrollToBlock = (block, blockId, duration) => {
+  let onVisibleFirst = true;
+
+  if (onVisibleFirst) {
+    ScrollTrigger.create({
+      trigger: block,
+      start: window.screen.width > 768 ? 'top 70%' : 'top 50%',
+      onEnter: () => {
+        gsap.to(window, { scrollTo: blockId, duration: 2 });
+        onVisibleFirst = false;
+      }
+    });
+  }
+};
 
 const weightTabs = () => {
   const tabsContainer = document.querySelector('.item-banner__weights'),
@@ -39,8 +56,6 @@ const catalogBannerAnimation = () => {
     duration = 3,
     ease = 'power2.in';
 
-  let onVisibleFirst = true;
-
   const timeline = gsap.timeline({
     scrollTrigger: {
       trigger: block,
@@ -48,18 +63,7 @@ const catalogBannerAnimation = () => {
     }
   });
 
-  if (onVisibleFirst) {
-    gsap.to(window, {
-      scrollTrigger: {
-        trigger: block,
-        start: window.screen.width > 768 ? 'top 80%' : 'top 50%'
-      },
-      duration,
-      scrollTo: '#catalog-block'
-    });
-
-    onVisibleFirst = false;
-  }
+  scrollToBlock(block, '#catalog-block', duration);
 
   timeline
     .from(title, {
@@ -231,6 +235,198 @@ const catalogBannerAnimation = () => {
     );
 };
 
+const loveAnimation = () => {
+  const block = document.querySelector('.love'),
+    titleLeft = block.querySelector('.love__title-left'),
+    titleRight = block.querySelector('.love__title-right'),
+    heart = block.querySelector('.love__heart'),
+    mainSlide = block.querySelector('.love__swiper-slide:first-child'),
+    slideBackground = block.querySelector('.love__swiper-background'),
+    sliderNavigation = block.querySelector('.love__swiper-navigation'),
+    tags = block.querySelectorAll('.love__tag'),
+    bottomLeft = block.querySelector('.love__bottom-text:first-child'),
+    bottomMiddle = block.querySelector('.love__bottom-swiper'),
+    bottomRight = block.querySelector('.love__bottom-text:last-child'),
+    duration = 3;
+
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: block,
+      start: 'top center'
+    }
+  });
+
+  scrollToBlock(block, '#love-block', duration);
+
+  timeline
+    .from(titleLeft, {
+      x: -1700,
+      duration,
+      keyframes: {
+        '0%': { x: -700 },
+        '40%': { x: 25 },
+        '100%': { x: 0 }
+      }
+    })
+    .from(
+      titleRight,
+      {
+        x: 1000,
+        duration,
+        keyframes: {
+          '0%': { x: 1000 },
+          '40%': { x: -25 },
+          '100%': { x: 0 }
+        }
+      },
+      0
+    )
+    .from(
+      heart,
+      {
+        y: -300,
+        opacity: 0,
+        scale: 2,
+        duration,
+        keyframes: {
+          '0%': { y: -300, opacity: 0, scale: 2.5 },
+          '40%': { y: 25, opacity: 1, scale: 1 },
+          '100%': { y: 0, opacity: 1, scale: 1 }
+        }
+      },
+      0
+    )
+    .from(
+      mainSlide,
+      {
+        y: 100,
+        duration,
+        keyframes: {
+          0: { y: 100 },
+          '40%': { y: -15, scale: 1.1 },
+          '100%': { y: 0, scale: 1 }
+        }
+      },
+      0
+    )
+    .from(
+      slideBackground,
+      {
+        y: 100,
+        scale: 0.5,
+        duration,
+        keyframes: {
+          '0%': { y: 100, scale: 0.5 },
+          '40%': { y: -15, scale: 1 },
+          '100%': { y: 0, scale: 1 }
+        }
+      },
+      0
+    )
+    .from(
+      sliderNavigation,
+      {
+        x: -500,
+        duration,
+        keyframes: {
+          '0%': { x: 500 },
+          '40%': { x: -25 },
+          '100%': { x: 0 }
+        }
+      },
+      0
+    )
+    .from(
+      tags[0],
+      {
+        y: -500,
+        opacity: 0,
+        rotateZ: -31,
+        duration,
+        keyframes: {
+          '0%': { y: -500, opacity: 0, rotateZ: -31 },
+          '40%': { y: 10, opacity: 1, rotateZ: 1 },
+          '100%': { y: 0, opacity: 1, rotateZ: -1 }
+        }
+      },
+      0
+    )
+    .from(
+      tags[1],
+      {
+        y: -470,
+        opacity: 0,
+        rotateZ: -31,
+        duration,
+        keyframes: {
+          '0%': { y: -470, opacity: 0, rotateZ: -31 },
+          '40%': { y: 10, opacity: 1, rotateZ: 12 },
+          '100%': { y: 0, opacity: 1, rotateZ: 9 }
+        }
+      },
+      0
+    )
+    .from(
+      tags[2],
+      {
+        y: -440,
+        opacity: 0,
+        rotateZ: -31,
+        duration,
+        keyframes: {
+          '0%': { y: -440, opacity: 0, rotateZ: -31 },
+          '40%': { y: 10, opacity: 1, rotateZ: -8 },
+          '100%': { y: 0, opacity: 1, rotateZ: -8 }
+        }
+      },
+      0
+    )
+    .from(
+      bottomLeft,
+      {
+        y: 100,
+        x: -50,
+        opacity: 0,
+        duration,
+        keyframes: {
+          '0%': { y: 100, x: -50, opacity: 0 },
+          '40%': { y: -10, x: 15, opacity: 1 },
+          '100%': { y: 0, x: 0, opacity: 1 }
+        }
+      },
+      0
+    )
+    .from(
+      bottomMiddle,
+      {
+        y: 100,
+        opacity: 0,
+        duration,
+        keyframes: {
+          '0%': { y: 100, opacity: 0 },
+          '40%': { y: -10, opacity: 1 },
+          '100%': { y: 0, opacity: 1 }
+        }
+      },
+      0
+    )
+    .from(
+      bottomRight,
+      {
+        y: 100,
+        x: 50,
+        opacity: 0,
+        duration,
+        keyframes: {
+          '0%': { y: 100, x: 50, opacity: 0 },
+          '40%': { y: -10, x: -15, opacity: 1 },
+          '100%': { y: 0, x: 0, opacity: 1 }
+        }
+      },
+      0
+    );
+};
+
 const Swipers = () => {
   const catalogBlockSwiper = new Swiper('.catalog-block__swiper', {
     slidesPerView: 1.2,
@@ -248,6 +444,89 @@ const Swipers = () => {
     navigation: {
       prevEl: '.catalog-block__swiper-btn--prev',
       nextEl: '.catalog-block__swiper-btn--next'
+    }
+  });
+
+  const mainSwiper = new Swiper('.love__swiper', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    speed: 1000,
+    effect: 'creative',
+
+    creativeEffect: {
+      prev: {
+        translate: [-1000, 0, 0],
+        scale: 0.1,
+        opacity: 0
+      },
+
+      next: {
+        translate: [1000, 0, 0],
+        scale: 0.1,
+        opacity: 0
+      }
+    },
+
+    navigation: {
+      nextEl: '.love__swiper-btn--next',
+      prevEl: '.love__swiper-btn--prev'
+    }
+  });
+
+  const tagsSwiper = new Swiper('.love__tags-swiper', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    speed: 1000,
+    effect: 'creative',
+
+    creativeEffect: {
+      prev: {
+        translate: [-500, 0, 0],
+        opacity: 0
+      },
+
+      next: {
+        translate: window.screen.width > 768 ? [0, '-150%', 0] : [500, 0, 0],
+        opacity: 0
+      }
+    },
+
+    navigation: {
+      nextEl: '.love__swiper-btn--next',
+      prevEl: '.love__swiper-btn--prev'
+    }
+  });
+
+  const textSwiper = new Swiper('.love__bottom-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    speed: 1000,
+    autoHeight: true,
+    effect: 'creative',
+
+    breakpoints: {
+      768: {
+        direction: 'vertical',
+        autoHeight: false
+      }
+    },
+
+    creativeEffect: {
+      prev: {
+        translate: window.screen.width > 768 ? [0, '-150%', 0] : [-500, 0, 0]
+      },
+
+      next: {
+        translate: window.screen.width > 768 ? [0, '100%', 0] : [500, 0, 0],
+        opacity: window.screen.width > 768 ? 0 : 1
+      }
+    },
+
+    navigation: {
+      nextEl: '.love__swiper-btn--next',
+      prevEl: '.love__swiper-btn--prev'
     }
   });
 };
